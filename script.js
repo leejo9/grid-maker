b935521a63c5b7f18d6c000be099496b3ad3283a
leej9 <minescoop04@gmail.com>
2024-09-30 22:58:34 -0400

    undo

--- script.js
+++ script.js
@@ -16,19 +16,7 @@ function addRow() {
     table.appendChild(row);
   }
 }
-function addCol() {
-  columns++;
-  let row = document.getElementById('dynamicTable').getElementsByTagName('tr');
-  for (let i = 0; i < rows; i++) {
-    const cell = document.createElement('td');
 
-    cell.onclick = function () {
-      colorChange(this);
-    };
-    row[i].appendChild(cell);
-
-  }
-}
 
 function resetTable() {
   table = document.getElementById('dynamicTable');
